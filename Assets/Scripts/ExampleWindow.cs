0be18051c33532c0193b8051e96dcc3a5e6cc56b
angelsophia1 <angelsunyulei@126.com>
2019-12-02 19:06:32 +0800

    Initial commit

---
+++ Assets/Scripts/ExampleWindow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class ExampleWindow : EditorWindow
+{
+    Color color;
+
+    [MenuItem("Window/Colorizer")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow<ExampleWindow>("Colorize");
+    }
+
+    private void OnGUI()
+    {
+        //Window Code
+        GUILayout.Label("Color the selected objects!", EditorStyles.boldLabel);
+
+        color = EditorGUILayout.ColorField("Color",color);
+
+        if (GUILayout.Button("Colorize"))
+        {
+            Colorize();
+        }
+    }
+    void Colorize()
+    {
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            Renderer renderer = obj.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                //var tempMaterial = new Material(renderer.sharedMaterial);
+                //tempMaterial.color = color;
+                //renderer.material = tempMaterial;
+                renderer.sharedMaterial.color = color;
+            }
+        }
+    }
+}
